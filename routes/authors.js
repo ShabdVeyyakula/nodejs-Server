14cee97a2ccd5cc634b99542f94fd2da59636b23
ShabdVeyyakula <47516044+ShabdVeyyakula@users.noreply.github.com>
2020-05-05 21:47:18 -0700

    error message

--- routes/authors.js
+++ routes/authors.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Author = require('../models/authors')
 
 
 //All Authors Route
@@ -9,13 +10,37 @@ router.get('/', (req, res) => {
 
 //New Author Route
 router.get('/new', (req, res) => {
-    res.render('authors/new')
+    res.render('authors/new', {author: new Author()})
 });
 
 
 //Create Author
 router.post('/', (req, res) => {
-    res.send('Create')
+    const author = new Author ({
+        name: req.body.name
+    });
+    try {
+        const newAuthor = author.save();
+        //res.redirect('authors/${newAuthor.id}`');
+        res.redirect(`authors`);
+
+    } catch {
+        res.render('authors/new', {
+            authors: author,
+            errorMessage: 'Error Creating Author'
+            })
+    }
+    // author.save((err, newAuthor) => {
+    //     if (err) {
+    //         res.render('authors/new', {
+    //             authors: author,
+    //             errorMessage: 'Error Creating Author'
+    //         })
+    //     } else {
+    //         //res.redirect('authors/${newAuthor.id}`')
+    //         res.redirect(`authors`);
+    //     }
+    // });
 });
 
 module.exports = router
